40d515b583dcf5fae30888fd0064ec4058a390c5
VinodhPIT <vinodh.mn@pitsolutions.com>
2023-09-23 11:39:11 +0530

    fixes

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,28 @@
-'use client'
+"use client";
 import { GlobalStateProvider } from "@/context/Context";
 import { Figtree } from "next/font/google";
 import Header from "@/app/components/header/header";
 import Footer from "@/app/components/footer/footer";
-import { usePathname } from 'next/navigation';
-import Layouts from '@/utils/Layout'
+import { usePathname } from "next/navigation";
+import Layouts from "@/utils/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './globals.css'
-
+import "./globals.css";
 
 const figtree = Figtree({
-
   style: ["normal"],
 
   weight: ["400", "700", "900", "600"],
 
   subsets: ["latin"],
-
 });
-
 
 export default function RootLayout({ children }) {
   const pathName = usePathname();
 
- 
-
   const shouldHideHeader = () => {
-
     // Specify the routes where you want to hide the header
 
     const hideHeaderRoutes = [
-
       "/search",
 
       "/",
@@ -46,44 +38,25 @@ export default function RootLayout({ children }) {
       "/joinartist",
 
       "/all/[detail]",
-
     ];
 
- 
-
     return hideHeaderRoutes.includes(pathName);
-
   };
 
- 
-
-  
   return (
     <html lang="en">
-
-    <GlobalStateProvider>
-
-      <body className={figtree.className}>
-
+      <GlobalStateProvider>
+        <body className={figtree.className}>
           <Layouts pathname={pathName}>
+            {!shouldHideHeader() && <Header />}
 
+            {children}
+            
+          </Layouts>
 
-      {!shouldHideHeader() && <Header /> }
-        
-        {children}
-        
-        
-        </Layouts>
-
-
-
-        <Footer/>
+          <Footer />
         </body>
-
-
       </GlobalStateProvider>
-
-
     </html>
-  )
+  );
 }
